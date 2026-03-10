e7f730c442bb095a7c0e9e8ff20a1c8f13822c82
HermianGamer <gianfranco.scarpatiperez@gmail.com>
2026-03-10 00:51:40 -0500

    added images

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import { netlifyTool } from 'sanity-plugin-netlify'
+
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +14,7 @@ export default defineConfig({
   projectId: 'iuu4f520',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), netlifyTool()],
 
   schema: {
     types: schemaTypes,
